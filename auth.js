050d12c97342ec66c972383dda93a46c458f6334
MOHAMMAD SHAAN <mdsworld2006@gmail.com>
2025-07-07 18:09:58 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,20 @@
+function login() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  auth.signInWithEmailAndPassword(email, password)
+    .then(() => {
+      if (email === 'admin@example.com') {
+        window.location.href = 'admin.html';
+      } else {
+        window.location.href = 'dashboard.html';
+      }
+    })
+    .catch(error => {
+      document.getElementById('loginStatus').innerText = error.message;
+    });
+}
+
+function logout() {
+  auth.signOut().then(() => window.location.href = 'index.html');
+}
